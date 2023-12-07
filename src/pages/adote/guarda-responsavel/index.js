1e635ae05ca11c7699b156d6bedddbe37a210ca1
rmagnusr <rmagnusdarosa@gmail.com>
2023-12-07 08:28:46 -0300

    responsible guard

---
+++ src/pages/adote/guarda-responsavel/index.js
@@ -0,0 +1,43 @@
+import Footer from '@/components/footer';
+import Header from '@/components/header';
+
+const ResponsibleGuard = () => {
+  return (
+    <>
+      <Header />
+      <main className="responsible-guard">
+        <div className="section-title">
+          <h2>- guarda responsável -</h2>
+          <p>
+            A guarda responsável é aquela em que as seguintes regras básicas são
+            seguidas, de forma a se garantir a saúde, a segurança e o conforto
+            dos animais
+          </p>
+        </div>
+        <p><span className="bold">1. </span> Ao decidir-se por acolher um animal, tenha em mente que ele viverá cerca de doze anos, ou mais, e que necessitará de seus cuidados, independentemente das mudanças que sua vida venha a sofrer no decorrer desse período;</p>
+        <p><span className="bold">2. </span>Prefira sempre adotar a comprar um animal. Ao adotar um animal, luta-se não só contra o abandono, mas contra o comércio de animais praticado por criadores, que se perfaz à custa de extrema crueldade. É preciso ter consciência de que adquirir um animal de criador implica, necessariamente, patrocinar o abusivo comércio de animais;</p>
+        <p><span className="bold">3. </span>Certifique-se de que poderá cuidar do animal durante o período de férias e no decorrer de feriados;</p>
+        <p><span className="bold">4. </span>Escolha o animal que possua características de comportamento e de tamanho condizentes com o espaço de que dispõe e com os seus próprios hábitos;</p>
+        <p><span className="bold">5. </span>Ministre-lhe assistência veterinária;</p>
+        <p><span className="bold">6. </span>Providencie para que seja o animal, macho ou fêmea, esterilizado para evitar crias indesejadas que resultam em abandono e em superpopulação de animais; – TODO ANIMAL DOADO PELA ONDA É DEVIDAMENTE CASTRADO E DESMIFURGADO.</p>
+        <p><span className="bold">7. </span>Vaciná-lo, anualmente, contra raiva, a partir dos 4 (quatro) meses de idade e contra as demais doenças (vacina V8), a partir dos 60 (sessenta) dias de vida;</p>
+        <p><span className="bold">8. </span>Não o abandonar em caso de doença, de idade avançada, de viagem, de agressividade ou de outra hipótese;</p>
+        <p><span className="bold">9. </span>Proporcionar-lhe alimentação adequada à espécie; gatos não devem ser alimentados com ração para cães e vice-versa;</p>
+        <p><span className="bold">10. </span>Proporcionar-lhe água fresca (água estagnada acumula larvas de mosquitos, que são prejudiciais à saúde);</p>
+        <p><span className="bold">11. </span>Provê-lo de espaço adequado, ao abrigo do sol e da chuva. Melhor é que se tenha o animal dentro de casa, mas se isso não for possível, dê-lhe ao menos uma casinha, que deve ser colocada ao abrigo do sol, da chuva e do vento;</p>
+        <p><span className="bold">12. </span>Não o prender a correntes, cordas ou a aparato similar. Dê ao animal um lar, e não uma prisão;</p>
+        <p><span className="bold">13. </span>Zelar para que o animal não fuja de casa, providenciando para que os portões de casa sejam resistentes e estejam sempre bem fechados, em caso de gatos deixá-los somente dentro de casa com passeios regulares em guia;</p>
+        <p><span className="bold">14. </span>Telar as janelas, caso more em prédio de apartamentos;</p>
+        <p><span className="bold">15. </span>Mantê-lo em boas condições de higiene (a água do banho deve ser quente);</p>
+        <p><span className="bold">16. </span>Jamais submetê-lo a maus-tratos, nem sob o pretexto de educá-lo;</p>
+        <p><span className="bold">17. </span>Passear com o animal para que ele se exercite, sempre preso à coleira e à guia para evitar fuga, atropelamento, ataques a outros animais, et cetera. Evite levá-lo para passear em horário de sol forte, pois o contato com o solo quente pode causar desconforto e até queimaduras;</p>
+        <p><span className="bold">18. </span>Dar afeto e atenção ao animal;</p>
+        <p><span className="bold">19. </span>Proporcionar-lhe conforto e espaço adequado; áreas descampadas, estacionamentos e garagens não são recomendáveis para animais;</p>
+        <p><span className="bold">20. </span>Amenizar-lhe a sensação de frio, por meio de roupas e cobertores; animais sentem frio tanto quanto os humanos.</p>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default ResponsibleGuard;
